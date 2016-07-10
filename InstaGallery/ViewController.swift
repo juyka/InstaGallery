9552f9ada266f8c3225b6cb004dc21e57e64f563
Анастасия Васюра <a.vasyura@rambler-co.ru>
2016-07-10 12:54:13 +0300

    Initial Commit

---
+++ InstaGallery/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InstaGallery
+//
+//  Created by Анастасия Васюра on 10/07/16.
+//  Copyright © 2016 Анастасия Васюра. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
